668f46cd567fa47dcd741aa9fe4868f4886bd5e5
Aviad Seady <aviadevelops@gmail.com>
2022-06-20 13:29:16 +0300

    Added the scenario

---
+++ mp3 steganography scenario/client.py
@@ -0,0 +1,31 @@
+import socket
+from mp3stego import Steganography
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+
+def receive_file():
+    mp3file_content = b''
+    while True:
+        data = sock.recv(1024)
+        mp3file_content += data
+        print(len(mp3file_content))
+        if not data:
+            break
+
+    return mp3file_content
+
+
+script = "ls -l\necho hello world"
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+
+    with open('test.mp3', 'rb') as mp3file:
+        s = Steganography(quiet=False)
+        s.hide_message('test.mp3', 'test_out.mp3', script)
+
+    with open('test_out.mp3', 'rb') as mp3file_out:
+        sock.send(mp3file_out.read())
+
+
---
+++ mp3 steganography scenario/server.py
@@ -0,0 +1,43 @@
+import os
+import socket
+
+from mp3stego import Steganography
+
+HOST = "127.0.0.1"  # localhost
+PORT = 65432  # Port to listen on
+
+
+def receive_file():
+    with conn:
+        mp3file_content = b''
+        while True:
+            data = conn.recv(1024)
+            mp3file_content += data
+            if not data:
+                break
+
+    return mp3file_content
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind((HOST, PORT))
+    sock.listen()
+    while True:
+        conn, addr = sock.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            mp3file_content = receive_file()
+
+        with open('file.mp3', 'wb') as mp3file:
+            print('Saving mp3 file')
+            mp3file.write(mp3file_content)
+            s = Steganography(quiet=False)
+            s.reveal_massage('file.mp3', 'msg.txt')
+            with open('msg.txt', 'r') as msg:
+                script_content = msg.read()
+                print('Hidden script:', script_content)
+                print("#" * 20)
+                with open('script.sh', 'w') as script:
+                    script.write(script_content)
+
+            os.system('sh script.sh')
